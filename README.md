ad823cc767c31f1b223fc0c6441a59854486a9e7
David Kotlirevsky <david.kotlirevsky@gmail.com>
2023-01-18 09:39:59 -0300

    Add Models and migrations

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-# DJANGO project MY APP
+# DJANGO project First Site - My Site
 
+Based on [Django, curso para principiantes](https://www.youtube.com/watch?v=T1intZyhXDU)
+
+## others
+
+de ser necesario bajar otros programas
+como 
+* [DB Browser for SQLlite](https://sqlitebrowser.org/)
 
 
--- myapp/urls.py
+++ myapp/urls.py
@@ -13,14 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
-
-import myapp
+from django.urls import path
+from myapp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # esun prefijo el home aca
-    path('home/', include('myapp.urls')),
-
+    path('', views.hello),
+    path('about/', views.about),
 ]
